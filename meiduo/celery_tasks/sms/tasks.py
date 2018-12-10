de469cb21ff94be427a8c9fcbc6698efbde01b2a
zzq <xwp_fullstack@163.com>
2018-12-10 17:18:58 +0800

    redis管道实现与celery异步任务发送短信

---
+++ meiduo/celery_tasks/sms/tasks.py
@@ -0,0 +1,6 @@
+from meiduo.libs.yuntongxun.sms import CCP
+from celery_tasks.main import app
+@app.task(name='send_sms_code')
+def send_sms_code(mobile,sms_code):
+    ccp=CCP()
+    ccp.send_template_sms(mobile,[sms_code,1],1)
--- meiduo/meiduo/apps/users/views.py
+++ meiduo/meiduo/apps/users/views.py
@@ -4,18 +4,31 @@ from random import randint
 from django_redis import get_redis_connection
 from rest_framework.response import Response
 from meiduo.libs.yuntongxun.sms import CCP
+from celery_tasks.sms.tasks import send_sms_code
 
 
 # Create your views here.
 class SMSCodeView(APIView):
     def get(self, request, mobile):
+        # 建立连接,得到连接对象
+        # 判断发送频率 控制再60s内不许再次发送
+        conn = get_redis_connection('verify')
+        flag = conn.get('sms_flag_%s' % mobile)
+        if flag:
+            return Response({'errors': '请求过于频繁'}, status=402)
         # 生成短信验证码
         sms_code = '%06d' % randint(0, 999999)
         # 保存短信验证码
-        conn = get_redis_connection('verify')
-        conn.setex('sms_%s' % mobile, 300, sms_code)
+        pl=conn.pipeline()
+        pl.setex('sms_%s' % mobile, 300, sms_code)
+        pl.setex('sms_flag_%s' % mobile, 60, 'zhazha')
+
+        pl.execute() # 传入指令,会执行写入redis命令
         # 发送短信验证码
-        ccp = CCP()
-        ccp.send_template_sms(mobile, [sms_code, '5'], 1)
+        # 通过celery发送短信
+        send_sms_code.delay(mobile,sms_code)
+        # ccp = CCP()
+        # ccp.send_template_sms(mobile, [sms_code, '5'], 1)
+
         # json 返回数据
         return Response({'message': 'OK'})
